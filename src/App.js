778e1c5890feefe2a3c7b94382ec9fddc38a89d2
Steven Olmos <eaodesigns@gmail.com>
2022-08-18 21:12:04 -0700

    The App Component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Container, Navbar, NavbarBrand } from "reactstrap";
+import NucampLogo from "./app/assets/img/logo.png";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      Hello, Nucamp!
+      <Navbar dark color="primary" sticky="top" expand="md">
+        <Container>
+          <NavbarBrand href="/">
+            <img src={NucampLogo} alt="nucamp logo" />
+          </NavbarBrand>
+        </Container>
+      </Navbar>
+      I'm ready for workshop!
     </div>
   );
 }
