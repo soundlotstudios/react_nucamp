16e84db0d8e94407cb61e7c4baf3edbe709bf507
Steven Olmos <eaodesigns@gmail.com>
2022-08-19 18:22:23 -0700

    Week 1 Workshop: Task 3

--- src/App.js
+++ src/App.js
@@ -1,20 +1,14 @@
-import React from "react";
-import { Container, Navbar, NavbarBrand } from "reactstrap";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
 import CampsitesList from "./features/campsites/CampsitesList";
-import NucampLogo from "./app/assets/img/logo.png";
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <Navbar dark color="primary" sticky="top" expand="md">
-        <Container>
-          <NavbarBrand href="/">
-            <img src={NucampLogo} alt="nucamp logo" />
-          </NavbarBrand>
-        </Container>
-      </Navbar>
+      <Header />
       <CampsitesList />
+      <Footer />
     </div>
   );
 }
