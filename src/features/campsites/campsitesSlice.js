367cf4016c33cceeafd5a70194da607cdc2fe7a3
Steven Olmos <eaodesigns@gmail.com>
2022-09-13 18:48:10 -0700

    Async Campsites

--- src/features/campsites/campsitesSlice.js
+++ src/features/campsites/campsitesSlice.js
@@ -1,13 +1,44 @@
-import { createSlice } from "@reduxjs/toolkit";
-import { CAMPSITES } from "../../app/shared/CAMPSITES";
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+// import { CAMPSITES } from '../../app/shared/CAMPSITES';
+import { baseUrl } from "../../app/shared/baseUrl";
+import { mapImageURL } from "../../utils/mapImageURL";
+
+export const fetchCampsites = createAsyncThunk(
+  "campsites/fetchCampsites",
+  async () => {
+    const response = await fetch(baseUrl + "campsites");
+    if (!response.ok) {
+      return Promise.reject("Unable to fetch, status: " + response.status);
+    }
+    const data = await response.json();
+    return data;
+  }
+);
 
 const initialState = {
-  campsitesArray: CAMPSITES,
+  campsitesArray: [],
+  isLoading: true,
+  errMsg: "",
 };
 
 const campsitesSlice = createSlice({
   name: "campsites",
   initialState,
+  reducers: {},
+  extraReducers: {
+    [fetchCampsites.pending]: (state) => {
+      state.isLoading = true;
+    },
+    [fetchCampsites.fulfilled]: (state, action) => {
+      state.isLoading = false;
+      state.errMsg = "";
+      state.campsitesArray = mapImageURL(action.payload);
+    },
+    [fetchCampsites.rejected]: (state, action) => {
+      state.isLoading = false;
+      state.errMsg = action.error ? action.error.message : "Fetch failed";
+    },
+  },
 });
 
 export const campsitesReducer = campsitesSlice.reducer;
@@ -16,12 +47,12 @@ export const selectAllCampsites = (state) => {
   return state.campsites.campsitesArray;
 };
 
-export const selectFeaturedCampsite = (state) => {
-    return state.campsites.campsitesArray.find((campsite) => campsite.featured);
-};
-
 export const selectCampsiteById = (id) => (state) => {
   return state.campsites.campsitesArray.find(
-      (campsite) => campsite.id === parseInt(id)
+    (campsite) => campsite.id === parseInt(id)
   );
+};
+
+export const selectFeaturedCampsite = (state) => {
+  return state.campsites.campsitesArray.find((campsite) => campsite.featured);
 };
